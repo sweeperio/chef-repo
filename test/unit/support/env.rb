2d95aed55d2b0039bf325bfe512ba0b6372ec6cd
pseudomuto <david.muto@gmail.com>
2016-01-10 12:44:49 -0500

    removing unused block param

--- test/unit/support/env.rb
+++ test/unit/support/env.rb
@@ -1,7 +1,7 @@
 shared_context "development environment", env: :development do
   let(:environment) { Chef::Environment.load_from_file("development") }
 
-  before(:each) do |example|
+  before do
     allow_any_instance_of(Chef::Node).to receive(:chef_environment).and_return(environment.name)
     allow(Chef::Environment).to receive(:load).with(environment.name).and_return(environment)
   end
